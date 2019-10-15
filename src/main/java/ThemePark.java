4a4445d36a9ea7b3925bdd6543a0d00a3e9b8688
Amanda Williamson <aselliott4@aol.co.uk>
2019-10-15 21:55:30 +0100

    visitor extensions added

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,17 @@
+import behaviours.IReviewed;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<IReviewed> reviews;
+
+    public ThemePark(){
+        this.reviews = new ArrayList<>();
+    }
+//
+//    public void getAllReviewed(){
+//        return reviews;
+//    }
+
 }
--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,5 +1,6 @@
 package people;
 
+import attractions.RollerCoaster;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +9,12 @@ import static org.junit.Assert.assertEquals;
 public class VisitorTest {
 
     Visitor visitor;
+    private RollerCoaster rollerCoaster;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        rollerCoaster = new RollerCoaster("Blue Ridge", 10);
     }
 
     @Test
@@ -27,5 +30,11 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void canVisitAttraction() {
+        visitor.visitAttraction(rollerCoaster);
+        assertEquals(1, visitor.visitedAttractionCount());
     }
 }
